c12f3b9bb85cbb27af156179008f23bac8c0c334
Adewale Sanusi <adewalesanusi@outlook.com>
2022-04-06 01:37:40 +0100

    Initial Commit

---
+++ magic-8-ball/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  magic-8-ball
+//
+//  Created by Adewale Sanusi on 06/04/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
